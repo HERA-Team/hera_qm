7827c73306573bca791183f20f9c273e7c28779f
Josh Dillon <jsdiillon@mit.edu>
2017-06-22 14:51:30 -0700

    Moving over xrfi and metrics from heroical.
    Moved over modules and made them installable with a setup.py. Moved
    over associated tests. hera_qm.metrics is only there for legacy
    purposes and should eventually be deprecated.

--- hera_qm/__init__.py
+++ hera_qm/__init__.py
@@ -1,4 +1,5 @@
 """init file for hera_qm."""
-import vis_metrics
-import cal_metrics
+#import vis_metrics
+#import cal_metrics
 import xrfi
+import metrics
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import glob
+import os.path as path
+from os import listdir
+
+__version__ = '0.0.0'
+
+setup_args = {
+    'name': 'hera_qm',
+    'author': 'HERA Team',
+    'url': 'https://github.com/HERA-Team/hera_qm',
+    'license': 'BSD',
+    'description': 'HERA Data Quality Metrics.',
+    'package_dir': {'hera_qm': 'hera_qm'},
+    'packages': ['hera_qm'],
+    #    'scripts': glob.glob('scripts/*'),
+    'version': __version__,
+    'package_data': {'hera_qm': ['data/*', 'data/*/*', 'calibrations/*']},
+    #    'install_requires': ['numpy>=1.10', 'scipy', 'pyuvdata', 'astropy>1.2', 'aipy']
+    #    'dependency_links': ['https://github.com/zakiali/omnical/tarball/master#egg=omnical-dev',]
+    'zip_safe': False,
+}
+
+
+if __name__ == '__main__':
+    apply(setup, (), setup_args)
