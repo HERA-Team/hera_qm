fb54824b6c8f3fbcd8312964e7e4e6b5bb3200e1
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-09-27 11:01:52 -0700

    Updated xrfi_run script, added xrfi_apply script

--- scripts/xrfi_apply.py
+++ scripts/xrfi_apply.py
@@ -4,9 +4,9 @@ import sys
 from hera_qm import utils
 from hera_qm import xrfi
 
-a = utils.get_metrics_ArgumentParser('xrfi_run')
+a = utils.get_metrics_ArgumentParser('xrfi_apply')
 args = a.parse_args()
 filename = args.filename
 history = ' '.join(sys.argv)
 
-xrfi.xrfi_run(filename, args, history)
+xrfi.xrfi_apply(filename, args, history)
