04c6aabafe73db501b579aca47da54f8631443d9
Adam Beardsley <adam.p.beardsley@gmail.com>
2018-07-17 15:11:43 -0700

    starting check warnings in uvflag tests

--- hera_qm/tests/test_uvflag.py
+++ hera_qm/tests/test_uvflag.py
@@ -38,6 +38,28 @@ def test_init_UVData():
     nt.assert_true('Flag object with type "baseline"' in uvf.history)
     nt.assert_true(hera_qm_version_str in uvf.history)
 
+def test_init_UVData_copy_flags():
+    uv = UVData()
+    uv.read_miriad(test_d_file)
+    uvf = uvtest.checkWarnings(UVFlag, [uv], {'copy_flags': True, 'mode': 'metric'},
+                               nwarnings=1, message='Copying flags to type=="baseline"')
+    uvf = UVFlag(uv, copy_flags=True, mode='metric')
+    nt.assert_false(hasattr(uvf, 'metric_array'))  # Should be flag due to copy flags
+    nt.assert_true(np.array_equal(uvf.flag_array, uv.flag_array))
+    nt.assert_true(uvf.weights_array.shape == uv.flag_array.shape)
+    nt.assert_true(np.all(uvf.weights_array == 0))
+    nt.assert_true(uvf.type == 'baseline')
+    nt.assert_true(uvf.mode == 'flag')
+    nt.assert_true(np.all(uvf.time_array == uv.time_array))
+    nt.assert_true(np.all(uvf.lst_array == uv.lst_array))
+    nt.assert_true(np.all(uvf.freq_array == uv.freq_array[0]))
+    nt.assert_true(np.all(uvf.polarization_array == uv.polarization_array))
+    nt.assert_true(np.all(uvf.baseline_array == uv.baseline_array))
+    nt.assert_true(np.all(uvf.ant_1_array == uv.ant_1_array))
+    nt.assert_true(np.all(uvf.ant_2_array == uv.ant_2_array))
+    nt.assert_true('Flag object with type "baseline"' in uvf.history)
+    nt.assert_true(hera_qm_version_str in uvf.history)
+
 def test_init_UVCal():
     uvc = UVCal()
     uvc.read_calfits(test_c_file)
@@ -55,6 +77,20 @@ def test_init_UVCal():
     nt.assert_true(np.all(uvf.polarization_array == uvc.jones_array))
     nt.assert_true(np.all(uvf.ant_array == uvc.ant_array))
     nt.assert_true('Flag object with type "antenna"' in uvf.history)
+    nt.assert_true(hera_qm_version_str in uvf.history)
+
+def test_init_cal_copy_flags():
+    uv = UVCal()
+    uv.read_calfits(test_c_file)
+    uvf = UVFlag(uv, mode='metric', copy_flags=True)
+    nt.assert_false(hasattr(uvf, 'metric_array'))  # Should be flag due to copy flags
+    nt.assert_true(np.array_equal(uvf.flag_array, uv.flag_array))
+    nt.assert_true(uvf.weights_array.shape == uv.flag_array.shape)
+    nt.assert_true(uvf.type == 'antenna')
+    nt.assert_true(uvf.mode == 'flag')
+    nt.assert_true(np.all(uvf.time_array == np.unique(uv.time_array)))
+    nt.assert_true(np.all(uvf.freq_array == uv.freq_array[0]))
+    nt.assert_true(np.all(uvf.polarization_array == uv.jones_array))
     nt.assert_true(hera_qm_version_str in uvf.history)
 
 def test_init_wf_uvd():
