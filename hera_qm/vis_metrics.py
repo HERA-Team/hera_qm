239520e0efe7666e313c2716ae6efb7242fc3ecd
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-08-07 09:55:54 -0700

    More PEP8 and Cij moved in last commit

--- hera_qm/vis_metrics.py
+++ hera_qm/vis_metrics.py
@@ -26,13 +26,12 @@ def check_noise_variance(data):
         ai = data.get_data((key[0], key[0], key[2])).real
         aj = data.get_data((key[1], key[1], key[2])).real
         ww = w[1:, 1:] * w[1:, :-1] * w[:-1, 1:] * w[:-1, :-1]
-        Cij[key] = (np.sum(np.abs(dd)**2, axis=0) / np.sum(dai * daj, axis=0) *
-                    (data.channel_width * data.integration_time))
         dd = (((d[:-1, :-1] - d[:-1, 1:]) - (d[1:, :-1] - d[1:, 1:])) * ww
               / np.sqrt(4))
         dai = (((ai[:-1, :-1] + ai[:-1, 1:]) + (ai[1:, :-1] + ai[1:, 1:])) * ww
                / 4)
         daj = (((aj[:-1, :-1] + aj[:-1, 1:]) + (aj[1:, :-1] + aj[1:, 1:])) * ww
                / 4)
-
+        Cij[key] = (np.sum(np.abs(dd)**2, axis=0) / np.sum(dai * daj, axis=0) *
+                    (data.channel_width * data.integration_time))
     return Cij
