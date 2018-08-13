b1dc8695f3cde2d6d70c3cdf409751ef95673a26
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2018-08-13 12:45:12 -0700

    Tests and i/o code for recursive dict reading/writing

--- hera_qm/metrics_io.py
+++ hera_qm/metrics_io.py
@@ -9,6 +9,7 @@ import os
 import h5py
 import warnings
 import numpy as np
+import copy
 
 
 def _recursively_save_dict_to_group(h5file, path, in_dict):
@@ -39,3 +40,80 @@ def _recursively_save_dict_to_group(h5file, path, in_dict):
         else:
             raise TypeError("Cannot save key {0} with type {1}"
                             .format(key, type(in_dict[key])))
+
+
+def write_metric_file(filename, input_dict):
+    """Convert the input dictionary into an HDF5 File."""
+    input_dict = copy.deepcopy(input_dict)
+    if filename.split('.')[-1] == 'json':
+        warnings.warn("JSON-type files can still be written "
+                      "but are no longer writen by default.\n"
+                      "Write to HDF5 format for future compatibility.")
+        for key in input_dict:
+            input_dict[key] = str(input_dict[key])
+        with open(filename, 'w') as outfile:
+            json.dump(input_dict, outfile, indent=4)
+    else:
+        if filename.split('.')[-1] not in ('hdf5', 'h5'):
+            filename += '.hdf5'
+
+        with h5py.File(filename, 'w') as f:
+            header = f.create_group('Header')
+
+            header['history'] = input_dict.pop('history',
+                                               'No History Provided')
+
+            header['version'] = input_dict.pop('version',
+                                               'No Version information')
+
+            # Create group for metrics data in file
+            mgrp = f.create_group('Metrics')
+            _recursively_save_dict_to_group(f, '/Metrics/', input_dict)
+
+
+def _recursively_load_dict_to_group(h5file, path):
+    """Recursively read the hdf5 file and create sub-dictionaries."""
+    out_dict = {}
+    for key, item in h5file[path].items():
+        if isinstance(item, h5py.Dataset):
+            out_dict[str(key)] = item.value
+        elif isinstance(item, h5py.Group):
+            out_dict[str(key)] = _recursively_load_dict_to_group(h5file,
+                                                                 (path + key
+                                                                  + '/'))
+    return out_dict
+
+
+def load_json_metrics(json_file):
+    """Load cut decisions and metrics from a JSON into python dictionary."""
+    with open(json_file, 'r') as infile:
+        jsonMetrics = json.load(infile)
+    gvars = {'nan': np.nan, 'inf': np.inf, '-inf': -np.inf, 'array': np.array}
+
+    metric_dict = {}
+    for key, val in jsonMetrics.items():
+        if (key == 'version') or (key == 'history'):
+            metric_dict[key] = str(val)
+        else:
+            try:
+                metric_dict[key] = eval(str(val), gvars)
+            except SyntaxError as sne:
+                if str(sne).strip() == ('unexpected EOF while parsing '
+                                        '(<string>, line 1)'):
+                    warnings.warn("The key: {0} could not be parsed, added "
+                                  "the value as a string".format(key))
+                    metric_dict[key] = str(val)
+    return metric_dict
+
+
+def load_metric_file(filename):
+    """Load the given hdf5 files name into a dictionary."""
+    if filename.split('.')[-1] == 'json':
+        warnings.warn("JSON-type files can still be read but are no longer "
+                      "writen by default.\n"
+                      "Write to HDF5 format for future compatibility.")
+        return load_json_metrics(filename)
+    with h5py.File(filename, 'r') as f:
+        metric_dict = _recursively_load_dict_to_group(f, '/Header/')
+        metric_dict.update(_recursively_load_dict_to_group(f, '/Metrics/'))
+    return metric_dict
--- hera_qm/tests/test_metrics_io.py
+++ hera_qm/tests/test_metrics_io.py
@@ -13,6 +13,8 @@ import h5py
 import warnings
 from hera_qm.data import DATA_PATH
 from hera_qm import metrics_io
+import pyuvdata.tests as uvtest
+from hera_qm.version import hera_qm_version_str
 
 
 class test_class(object):
@@ -26,9 +28,9 @@ class test_class(object):
 def test_recursive_error_for_object_arrays():
     """Test a TypeError is raised if dictionary items are np.arrays of objecsts."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    path = '/'
+    bad_dict = {'1': np.array(['123', 1, np.pi], dtype='object')}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        bad_dict = {'1': np.array(['123', 1, np.pi], dtype='object')}
         nt.assert_raises(TypeError, metrics_io._recursively_save_dict_to_group,
                          h5_test, path, bad_dict)
     os.remove(test_file)
@@ -37,9 +39,9 @@ def test_recursive_error_for_object_arrays():
 def test_recursive_error_for_dict_of_object():
     """Test a TypeError is raised if dictionary items are objects."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    path = '/'
+    bad_dict = {'0': test_class()}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        bad_dict = {'0': test_class()}
         nt.assert_raises(TypeError, metrics_io._recursively_save_dict_to_group,
                          h5_test, path, bad_dict)
     os.remove(test_file)
@@ -48,9 +50,9 @@ def test_recursive_error_for_dict_of_object():
 def test_recursive_error_for_object_in_nested_dict():
     """Test TypeError is raised if object is nested in dict."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    path = '/'
+    bad_dict = {'0': {'0.0': test_class()}}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        bad_dict = {'0': {'0.0': test_class()}}
         nt.assert_raises(TypeError, metrics_io._recursively_save_dict_to_group,
                          h5_test, path, bad_dict)
     os.remove(test_file)
@@ -60,9 +62,9 @@ def test_recursive_adds_numpy_array_to_h5file():
     """Test that proper numpy arrays are added to h5file."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
     test_array = np.arange(10)
+    path = '/'
+    good_dict = {'0': test_array}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        good_dict = {'0': test_array}
         metrics_io._recursively_save_dict_to_group(h5_test, path, good_dict)
 
         nt.assert_true(np.allclose(test_array, h5_test['0'].value))
@@ -73,12 +75,10 @@ def test_recursive_adds_nested_numpy_array_to_h5file():
     """Test that a numpy array nested in a dict is added to h5file."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
     test_array = np.arange(10)
+    path = '/'
+    good_dict = {'0': {'1': test_array}}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        good_dict = {'0': {'1': test_array}}
         metrics_io._recursively_save_dict_to_group(h5_test, path, good_dict)
-        for key in h5_test:
-            print(key, h5_test[key])
         nt.assert_true(np.allclose(test_array, h5_test['0/1'].value))
     os.remove(test_file)
 
@@ -87,9 +87,9 @@ def test_recursive_adds_scalar_to_h5file():
     """Test that a scalar type is added to h5file."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
     test_scalar = 'hello world'
+    path = '/'
+    good_dict = {'0': test_scalar}
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        good_dict = {'0': test_scalar}
         metrics_io._recursively_save_dict_to_group(h5_test, path, good_dict)
 
         nt.assert_equal(test_scalar, h5_test['0'].value)
@@ -100,10 +100,139 @@ def test_recursive_adds_nested_scalar_to_h5file():
     """Test that a scalar nested in a dict is added to h5file."""
     test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
     test_scalar = 'hello world'
+    good_dict = {'0': {'1': test_scalar}}
+    path = '/'
     with h5py.File(test_file, 'w') as h5_test:
-        path = '/'
-        good_dict = {'0': {'1': test_scalar}}
         metrics_io._recursively_save_dict_to_group(h5_test, path, good_dict)
 
         nt.assert_equal(test_scalar, h5_test['0/1'].value)
     os.remove(test_file)
+
+
+def test_write_metric_file_hdf5():
+    """Test that correct hdf4 structure created from write_metric_file."""
+    test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    test_scalar = 'hello world'
+    test_array = np.arange(10)
+    test_dict = {'0': test_scalar, 1: {'0': test_scalar, '1': test_array}}
+    path = '/'
+    metrics_io.write_metric_file(test_file, test_dict)
+
+    with h5py.File(test_file, 'r') as test_h5:
+        nt.assert_equal(test_scalar, test_h5['/Metrics/0'].value)
+        nt.assert_equal(test_scalar, test_h5['Metrics/1/0'].value)
+        nt.assert_true(np.allclose(test_array, test_h5['Metrics/1/1'].value))
+    os.remove(test_file)
+
+
+def test_write_metric_file_hdf5_no_appelation_exists():
+    """Test hdf5 file created from write_metric_file if no appelation given."""
+    test_file = os.path.join(DATA_PATH, 'test_output', 'test')
+    test_scalar = 'hello world'
+    test_array = np.arange(10)
+    test_dict = {'0': test_scalar, 1: {'0': test_scalar, '1': test_array}}
+    path = '/'
+    metrics_io.write_metric_file(test_file, test_dict)
+    test_file += '.hdf5'
+    nt.assert_true(os.path.exists(test_file))
+    os.remove(test_file)
+
+
+def test_write_metric_warning_json():
+    """Test the known warning is issued when writing to json."""
+    json_file = os.path.join(DATA_PATH, 'test_output', 'test_save.json')
+    test_scalar = 'hello world'
+    test_array = np.arange(10)
+    test_dict = {'0': test_scalar, 1: {'0': test_scalar, '1': test_array}}
+    warn_message = ["JSON-type files can still be written but are no longer "
+                    "writen by default.\n"
+                    "Write to HDF5 format for future compatibility."]
+    json_dict = uvtest.checkWarnings(metrics_io.write_metric_file,
+                                     func_args=[json_file, test_dict],
+                                     category=UserWarning, nwarnings=1,
+                                     message=warn_message)
+    nt.assert_true(os.path.exists(json_file))
+    os.remove(json_file)
+
+
+def test_write_then_recursive_load_dict_to_group_no_nested_dicts():
+    """Test recursive load can gather dictionary from a group."""
+    test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    test_scalar = 'hello world'
+    path = '/'
+    good_dict = {'0': test_scalar, 'history': "this is a test",
+                 'version': hera_qm_version_str}
+    metrics_io.write_metric_file(test_file, good_dict)
+    with h5py.File(test_file, 'r') as h5file:
+        read_dict = metrics_io._recursively_load_dict_to_group(h5file, '/Header/')
+        read_dict.update(metrics_io._recursively_load_dict_to_group(h5file, '/Metrics/'))
+    nt.assert_dict_equal(good_dict, read_dict)
+    os.remove(test_file)
+
+
+def test_write_then_recursive_load_dict_to_group_with_nested_dicts():
+    """Test recursive load can gather dictionary from a  nested group."""
+    test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    test_scalar = 'hello world'
+    path = '/'
+    good_dict = {'0': test_scalar, 'history': "this is a test",
+                 'version': hera_qm_version_str, '1': {'0': test_scalar}}
+    metrics_io.write_metric_file(test_file, good_dict)
+    with h5py.File(test_file, 'r') as h5file:
+        read_dict = metrics_io._recursively_load_dict_to_group(h5file, '/Header/')
+        read_dict.update(metrics_io._recursively_load_dict_to_group(h5file, '/Metrics/'))
+    for key in good_dict:
+        if isinstance(good_dict[key], dict):
+            nt.assert_dict_equal(good_dict[key], read_dict[key])
+        else:
+            nt.assert_equal(good_dict[key], read_dict[key])
+    os.remove(test_file)
+
+
+def test_write_then_load_metric_file_hdf5():
+    """Test loaded in map is same as written one from hdf5."""
+    test_file = os.path.join(DATA_PATH, 'test_output', 'test.h5')
+    test_scalar = 'hello world'
+    path = '/'
+    good_dict = {'0': test_scalar, 'history': "this is a test",
+                 'version': hera_qm_version_str, '1': {'0': test_scalar}}
+    metrics_io.write_metric_file(test_file, good_dict)
+    read_dict = metrics_io.load_metric_file(test_file)
+    for key in good_dict:
+        if isinstance(good_dict[key], dict):
+            nt.assert_dict_equal(good_dict[key], read_dict[key])
+        else:
+            nt.assert_equal(good_dict[key], read_dict[key])
+    os.remove(test_file)
+
+
+def test_write_then_load_metric_warning_json_():
+    """Test the known warning is issued when writing to json."""
+    json_file = os.path.join(DATA_PATH, 'test_output', 'test_save.json')
+    test_scalar = "hello world"
+    test_array = np.arange(10)
+    test_dict = {'history': 'Test case', 'version': '0.0.0',
+                 '0': test_scalar, '1': {'0': str(test_scalar),
+                                         '1': test_array}}
+    warn_message = ["JSON-type files can still be read but are no longer "
+                    "writen by default.\n"
+                    "Write to HDF5 format for future compatibility.",
+                    "The key: 0 could not be parsed, added "
+                    "the value as a string"]
+    output_json = metrics_io.write_metric_file(json_file, test_dict)
+    json_dict = uvtest.checkWarnings(metrics_io.load_metric_file,
+                                     func_args=[json_file],
+                                     category=UserWarning, nwarnings=2,
+                                     message=warn_message)
+    for key in test_dict:
+        if isinstance(test_dict[key], dict):
+            for key1 in test_dict[key]:
+                if isinstance(test_dict[key][key1], np.ndarray):
+                    nt.assert_true(np.allclose(test_dict[key][key1],
+                                               json_dict[key][key1]))
+                else:
+                    nt.assert_equal(test_dict[key][key1], json_dict[key][key1])
+
+        else:
+            nt.assert_equal(test_dict[key], json_dict[key])
+    os.remove(json_file)
