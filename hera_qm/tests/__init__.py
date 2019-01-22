47fbfb5ab8de16c0ae2f5ab534d839a504812faf
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2019-01-22 16:27:47 -0700

    typo fix and better errors in recursive_compare_dicts

--- hera_qm/tests/__init__.py
+++ hera_qm/tests/__init__.py
@@ -35,7 +35,7 @@ def recursive_compare_dicts(d1, d2):
             except (AssertionError, TypeError) as err:
                 print('d1:', key, 'key value type:', type(d1[key]),
                       'data type:', type(d1[key][0]), d1[key])
-                print('d2:', key, 'key value type', type(d1[key]),
+                print('d2:', key, 'key value type:', type(d1[key]),
                       'data type:', type(d2[key][0]), d2[key])
                 raise err
         elif isinstance(d1[key], (np.ndarray)):
@@ -47,7 +47,7 @@ def recursive_compare_dicts(d1, d2):
                 except (AssertionError, TypeError) as err:
                     print('d1:', key, 'key value type:', type(d1[key]),
                           'data type:', type(d1[key][0]), d1[key])
-                    print('d2:', key, 'key value type', type(d1[key]),
+                    print('d2:', key, 'key value type:', type(d1[key]),
                           'data type:', type(d2[key][0]), d2[key])
                     raise err
         elif isinstance(d1[key], dict):
@@ -55,4 +55,11 @@ def recursive_compare_dicts(d1, d2):
         elif isinstance(d1[key], (float, np.float, np.float32)):
             nt.assert_true(np.allclose(d1[key], d2[key]))
         else:
-            nt.assert_equal(d1[key], d2[key])
+            try:
+                nt.assert_equal(d1[key], d2[key])
+            except (AssertionError, TypeError) as err:
+                print('d1:', key, 'key value type:', type(d1[key]),
+                      'data type:', type(d1[key][0]), d1[key])
+                print('d2:', key, 'key value type:', type(d1[key]),
+                      'data type:', type(d2[key][0]), d2[key])
+                raise err
