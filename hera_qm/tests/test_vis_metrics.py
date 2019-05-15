3a802744ecb2f1bede7cfcc47a9c92ba2ee98e38
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-05-15 14:41:12 -0700

    vis_metrics removed unittest and test classes

--- hera_qm/tests/test_vis_metrics.py
+++ hera_qm/tests/test_vis_metrics.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2019 the HERA Project
 # Licensed under the MIT License
 import hera_qm.tests as qmtest
-import unittest
 from hera_qm import vis_metrics
 import numpy as np
 from pyuvdata import UVData
@@ -14,46 +13,50 @@ import pytest
 from scipy import stats
 
 
-class TestMethods(unittest.TestCase):
+@pytest.fixture()
+def vismetrics_data():
+    data = UVData()
+    filename = os.path.join(DATA_PATH, 'zen.2457698.40355.xx.HH.uvcAA')
+    data.read_miriad(filename)
+    # massage the object to make it work with check_noise_variance
+    data.select(antenna_nums=data.get_ants()[0:10])
+    data.select(freq_chans=range(100))
+    # Data file only has three times... need more.
+    while data.Ntimes < 90:
+        d2 = copy.deepcopy(data)
+        d2.time_array += d2.time_array.max() + d2.integration_time / (24 * 3600)
+        data += d2
+    ntimes = data.Ntimes
+    nchan = data.Nfreqs
+    data1 = qmtest.noise(size=(ntimes, nchan))
+    data2 = qmtest.noise(size=(ntimes, nchan))
+    ant_dat = {}
+    for i in data.get_ants():
+        ant_dat[i] = qmtest.noise(size=(ntimes, nchan)) + 0.1 * data1
+    for key in data.get_antpairpols():
+        ind = data._key2inds(key)[0]
+        data.data_array[ind, 0, :, 0] = ant_dat[key[0]] * ant_dat[key[1]].conj()
+    return data, data1, data2
 
-    def setUp(self):
-        self.data = UVData()
-        filename = os.path.join(DATA_PATH, 'zen.2457698.40355.xx.HH.uvcAA')
-        self.data.read_miriad(filename)
-        # massage the object to make it work with check_noise_variance
-        self.data.select(antenna_nums=self.data.get_ants()[0:10])
-        self.data.select(freq_chans=range(100))
-        # Data file only has three times... need more.
-        while self.data.Ntimes < 90:
-            d2 = copy.deepcopy(self.data)
-            d2.time_array += d2.time_array.max() + d2.integration_time / (24 * 3600)
-            self.data += d2
-        ntimes = self.data.Ntimes
-        nchan = self.data.Nfreqs
-        self.data1 = qmtest.noise(size=(ntimes, nchan))
-        self.data2 = qmtest.noise(size=(ntimes, nchan))
-        ant_dat = {}
-        for i in self.data.get_ants():
-            ant_dat[i] = qmtest.noise(size=(ntimes, nchan)) + 0.1 * self.data1
-        for key in self.data.get_antpairpols():
-            ind = self.data._key2inds(key)[0]
-            self.data.data_array[ind, 0, :, 0] = ant_dat[key[0]] * ant_dat[key[1]].conj()
 
-    def test_check_noise_variance(self):
-        nos = vis_metrics.check_noise_variance(self.data)
-        for bl in self.data.get_antpairs():
-            inds = self.data.antpair2ind(*bl)
-            n = nos[bl + (uvutils.parse_polstr('xx'),)]
-            self.assertEqual(n.shape, (self.data.Nfreqs - 1,))
-            nsamp = self.data.channel_width * self.data.integration_time[inds][0]
-            np.testing.assert_almost_equal(n, np.ones_like(n) * nsamp,
-                                           -np.log10(nsamp))
+def test_check_noise_variance(vismetrics_data):
+    data, data1, data2 = vismetrics_data
+    nos = vis_metrics.check_noise_variance(data)
+    for bl in data.get_antpairs():
+        inds = data.antpair2ind(*bl)
+        n = nos[bl + (uvutils.parse_polstr('xx'),)]
+        assert n.shape == (data.Nfreqs - 1,)
+        nsamp = data.channel_width * data.integration_time[inds][0]
+        np.testing.assert_almost_equal(n, np.ones_like(n) * nsamp,
+                                       -np.log10(nsamp))
 
-    def test_check_noise_variance_inttime_error(self):
-        self.data.integration_time = (self.data.integration_time
-                                      * np.arange(self.data.integration_time.size))
-        self.assertRaises(NotImplementedError,
-                          vis_metrics.check_noise_variance, self.data)
+
+def test_check_noise_variance_inttime_error(vismetrics_data):
+    data, data1, data2 = vismetrics_data
+    data.integration_time = (data.integration_time
+                             * np.arange(data.integration_time.size))
+    pytest.raises(NotImplementedError,
+                  vis_metrics.check_noise_variance, data)
 
 
 def test_vis_bl_cov():
@@ -197,7 +200,3 @@ def test_sequential_diff():
     assert uvd_diff.Nfreqs == uvd.Nfreqs
     assert uvd_diff.flag_array[:, 0, -1, 0].all()
     assert uvd_diff.select(times=np.unique(uvd_diff.time_array)[-1:], inplace=False).flag_array.all()
-
-
-if __name__ == '__main__':
-    unittest.main()
