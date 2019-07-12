c6b16120a0f45d10b8629dd525588c95dea91102
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2019-07-12 12:02:54 -0700

    added conftest and IERS workaround from pyuvdata

---
+++ hera_qm/tests/conftest.py
@@ -0,0 +1,32 @@
+# -*- mode: python; coding: utf-8 -*
+# Copyright (c) 2019 the HERA Project
+# Licensed under the MIT License
+
+"""Testing environment setup and teardown for pytest."""
+from __future__ import absolute_import, division, print_function
+
+import os
+import pytest
+import shutil
+import six.moves.urllib as urllib
+from astropy.utils import iers
+from astropy.time import Time
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup_and_teardown_package():
+    """Make data/test directory to put test output files in."""
+
+    # try to download the iers table. If it fails, turn off auto downloading for the tests
+    # and turn it back on in teardown_package (done by extending auto_max_age)
+    try:
+        iers_a = iers.IERS_A.open(iers.IERS_A_URL)
+        t1 = Time.now()
+        t1.ut1
+    except(urllib.error.URLError):
+        iers.conf.auto_max_age = None
+
+    # yield to allow tests to run
+    yield
+
+    iers.conf.auto_max_age = 30
