bcff4f6a044632796c964e5d3b3b2e6842571766
Josh Dillon <jsdillon@berkeley.edu>
2020-08-09 13:59:14 -0700

    fix tests which tried to use history positionally

--- scripts/xrfi_run.py
+++ scripts/xrfi_run.py
@@ -13,7 +13,7 @@ history = ' '.join(sys.argv)
 
 xrfi.xrfi_run(args.ocalfits_files, args.acalfits_files, args.model_files,
               args.data_files, a_priori_flag_yaml=args.a_priori_flag_yaml,
-              history, xrfi_path=args.xrfi_path, throw_away_edges=not(args.keep_edge_times),
+              history=history, xrfi_path=args.xrfi_path, throw_away_edges=not(args.keep_edge_times),
               kt_size=args.kt_size, kf_size=args.kf_size, sig_init=args.sig_init,
               sig_adj=args.sig_adj, ex_ants=args.ex_ants,
               Nwf_per_load=args.Nwf_per_load,
