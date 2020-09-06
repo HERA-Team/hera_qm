3b88d10fe1dd49f156356df49cc2368289f625ba
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-09-05 17:04:38 -0700

    replace count_nonzero with any

--- hera_qm/utils.py
+++ hera_qm/utils.py
@@ -724,12 +724,12 @@ def apply_yaml_flags(uv, a_priori_flag_yaml, lat_lon_alt_degrees=None, telescope
             antnum = ant[0]
         if issubclass(uv.__class__, UVData):
             blt_selection = np.logical_or(uv.ant_1_array == antnum, uv.ant_2_array == antnum)
-            if np.count_nonzero(blt_selection) > 0:
+            if np.any(blt_selection):
                 for bltind in np.where(blt_selection)[0]:
                     uv.flag_array[bltind, :, :, pol_selection] = True
         elif issubclass(uv.__class__, UVCal):
             ant_selection = uv.ant_array == antnum
-            if np.count_nonzero(ant_selection) > 0:
+            if np.any(ant_selection):
                 for antind in np.where(ant_selection)[0]:
                     uv.flag_array[antind, :, :, :, pol_selection] =True
     # return uv with flags applied.
