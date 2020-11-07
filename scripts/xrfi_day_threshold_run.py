bce9d531370c79726e1cfb952e34a2b373c07613
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-11-07 13:05:25 -0800

    updated parser and script.

--- scripts/xrfi_day_threshold_run.py
+++ scripts/xrfi_day_threshold_run.py
@@ -13,6 +13,7 @@ history = ' '.join(sys.argv)
 
 if args.run_if_first is None or sorted(args.data_files)[0] == args.run_if_first:
     xrfi.day_threshold_run(args.data_files, history, nsig_f=args.nsig_f, nsig_t=args.nsig_t, flag_abscal=not(args.skip_making_flagged_abs_calfits),
-                           nsig_f_adj=args.nsig_f_adj, nsig_t_adj=args.nsig_t_adj, clobber=args.clobber)
+                           nsig_f_adj=args.nsig_f_adj, nsig_t_adj=args.nsig_t_adj, clobber=args.clobber,
+                           a_priori_flag_yaml=args.a_priori_flag_yaml)
 else:
     print(sorted(args.data_files)[0], 'is not', args.run_if_first, '...skipping.')
