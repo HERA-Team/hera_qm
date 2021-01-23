ff0ee76fa4f2d478f7a6a817bf6b38e11662eceb
Josh Dillon <jsdillon@berkeley.edu>
2021-01-23 10:37:40 -0800

    add useful statistical functions

--- hera_qm/auto_metrics.py
+++ hera_qm/auto_metrics.py
@@ -6,3 +6,20 @@
 import numpy as np
 from copy import deepcopy
 from .version import hera_qm_version_str
+
+
+def nanmad(a, axis=None):
+    '''Analogous to np.nanmedian, but for median absolute deviation.'''
+    nanmed = np.nanmedian(a, axis=axis, keepdims=True)
+    return np.nanmedian(np.abs(a - nanmed), axis=axis)
+
+def nanmedian_abs_diff(a, axis=0):
+    '''Computes the absolute difference between neightbors along axis, then collaposes 
+    along that axis with the nanmedian. Useful for studying temporal variability, e.g.'''
+    return np.nanmedian(np.abs(np.diff(a, axis=axis)), axis=axis)
+
+def nanmean_abs_diff(a, axis=0):
+    '''Computes the absolute difference between neightbors along axis, then collaposes 
+    along that axis with the nanmean. Useful for studying temporal variability, e.g.'''
+    return np.nanmean(np.abs(np.diff(a, axis=axis)), axis=axis)
+
