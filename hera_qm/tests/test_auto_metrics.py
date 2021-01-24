8b646ee8c2e83156a12a8765568216981aa006ec
Josh Dillon <jsdillon@berkeley.edu>
2021-01-24 14:27:44 -0800

    Create test_auto_metrics.py

---
+++ hera_qm/tests/test_auto_metrics.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 the HERA Project
+# Licensed under the MIT License
+"""Tests for the antenna_metrics module."""
+
+import pytest
+import numpy as np
+import os
+from hera_qm import auto_metrics
+from hera_qm import metrics_io
+from hera_qm.data import DATA_PATH
+import hera_qm.tests as qmtest
+
+
