829552312def502154b216c6673cad6dc3a0e24d
Josh Dillon <jsdillon@berkeley.edu>
2021-01-25 12:13:12 -0800

    add test_get_auto_spectra

--- hera_qm/tests/test_auto_metrics.py
+++ hera_qm/tests/test_auto_metrics.py
@@ -52,3 +52,56 @@ def test_check_only_auto_keys():
     bad_data = {(0, 0, 'ee'): np.zeros((10, 10)), (0, 0, 'ne'): np.zeros((10, 10))}
     with pytest.raises(ValueError):
         auto_metrics._check_only_auto_keys(bad_data)
+
+
+def test_get_auto_spectra():
+    autos = {(0, 0, 'ee'): 10 * np.ones((10, 5)), (1, 1, 'ee'): 10 * np.ones((10, 5))}
+    autos[0, 0, 'ee'][:, 1] = 20
+    autos[0, 0, 'ee'][0, 4] = 20
+
+    # test basic operation
+    spectra = auto_metrics.get_auto_spectra(autos)
+    np.testing.assert_array_equal(spectra[0, 0, 'ee'], np.array([1., 2., 1., 1., 1.]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], np.array([1., 1., 1., 1., 1.]))
+
+    # test time_avg_func=np.nanmean
+    spectra = auto_metrics.get_auto_spectra(autos, time_avg_func=np.nanmean)
+    np.testing.assert_array_equal(spectra[0, 0, 'ee'], np.array([1., 2., 1., 1., 1.1]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], np.array([1., 1., 1., 1., 1.]))
+
+    # test turning off scalar_norm
+    spectra = auto_metrics.get_auto_spectra(autos, time_avg_func=np.nanmean, scalar_norm=False)
+    np.testing.assert_array_equal(spectra[0, 0, 'ee'], 10 * np.array([1., 2., 1., 1., 1.1]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], 10 * np.array([1., 1., 1., 1., 1.]))
+
+    # test replacing norm_func
+    spectra = auto_metrics.get_auto_spectra(autos, time_avg_func=np.nanmean, norm_func=np.nanmean)
+    np.testing.assert_array_almost_equal(spectra[0, 0, 'ee'], 50 / 61 * np.array([1., 2., 1., 1., 1.1]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], np.array([1., 1., 1., 1., 1.]))
+
+    # test turning on waterfall norm with ex_ants
+    spectra = auto_metrics.get_auto_spectra(autos, time_avg_func=np.nanmean, norm_func=np.nanmean,
+                                            scalar_norm=False, waterfall_norm=True, ex_ants=[0])
+    np.testing.assert_array_almost_equal(spectra[0, 0, 'ee'], np.array([1., 2., 1., 1., 1.1]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], np.array([1., 1., 1., 1., 1.]))
+
+    # test turning on waterfall norm without ex_ants
+    spectra = auto_metrics.get_auto_spectra(autos, time_avg_func=np.nanmean, norm_func=np.nanmean,
+                                            scalar_norm=False, waterfall_norm=True)
+    np.testing.assert_array_almost_equal(spectra[0, 0, 'ee'], np.array([1., 4. / 3, 1., 1., 31. / 30]))
+    np.testing.assert_array_almost_equal(spectra[1, 1, 'ee'], np.array([1., 2. / 3, 1., 1., 29. / 30]))
+
+    # test with flags
+    flags = np.zeros((10, 5), dtype=bool)
+    flags[0, 4] = True
+    spectra = auto_metrics.get_auto_spectra(autos, flag_wf=flags, time_avg_func=np.nanmean, scalar_norm=False)
+    np.testing.assert_array_equal(spectra[0, 0, 'ee'], 10 * np.array([1., 2., 1., 1., 1.]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], 10 * np.array([1., 1., 1., 1., 1.]))
+
+    # test with totally flagged channel
+    flags[:, 1] = True
+    spectra = auto_metrics.get_auto_spectra(autos, flag_wf=flags, time_avg_func=np.nanmean, scalar_norm=False)
+    np.testing.assert_array_equal(spectra[0, 0, 'ee'], 10 * np.array([1., np.nan, 1., 1., 1.]))
+    np.testing.assert_array_equal(spectra[1, 1, 'ee'], 10 * np.array([1., np.nan, 1., 1., 1.]))
+
+
