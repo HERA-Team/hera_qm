b94d2260e48d3e1346942c0ef0c47a0031f490fa
Josh Dillon <jsdillon@berkeley.edu>
2023-04-10 16:03:25 -0700

    fix call to tested function

--- hera_qm/tests/test_time_series_metrics.py
+++ hera_qm/tests/test_time_series_metrics.py
@@ -9,18 +9,19 @@ from hera_qm import time_series_metrics
 
 
 def test_true_stretches():
-    assert time_series_metricstrue_stretches(np.array([True, False, False])) == [slice(0, 1, None)]
-    assert time_series_metricstrue_stretches(np.array([True, False, False, True])) == [slice(0, 1, None), slice(3, 4, None)]
-    assert time_series_metricstrue_stretches(np.array([False, True, True, False])) == [slice(1, 3, None)]
+    assert time_series_metrics.true_stretches(np.array([True, False, False])) == [slice(0, 1, None)]
+    assert time_series_metrics.true_stretches(np.array([True, False, False, True])) == [slice(0, 1, None), slice(3, 4, None)]
+    assert time_series_metrics.true_stretches(np.array([False, True, True, False])) == [slice(1, 3, None)]
+
 
 def test_impose_max_flag_gap():
-    np.testing.assert_array_equal(impose_max_flag_gap(np.array([False, True, False, True, False, False]), max_flag_gap=0),
+    np.testing.assert_array_equal(time_series_metrics.impose_max_flag_gap(np.array([False, True, False, True, False, False]), max_flag_gap=0),
                                   np.array([True, True, True, True, False, False]))
-    np.testing.assert_array_equal(impose_max_flag_gap(np.array([False, False, True, False, True, False]), max_flag_gap=0),
+    np.testing.assert_array_equal(time_series_metrics.impose_max_flag_gap(np.array([False, False, True, False, True, False]), max_flag_gap=0),
                                   np.array([False, False, True, True, True, True]))
-    np.testing.assert_array_equal(impose_max_flag_gap(np.array([False, False, True, False, True, False]), max_flag_gap=1),
+    np.testing.assert_array_equal(time_series_metrics.impose_max_flag_gap(np.array([False, False, True, False, True, False]), max_flag_gap=1),
                                   np.array([False, False, True, False, True, False]))
-    np.testing.assert_array_equal(impose_max_flag_gap(np.array([False, False, True, True, True, False]), max_flag_gap=2),
+    np.testing.assert_array_equal(time_series_metrics.impose_max_flag_gap(np.array([False, False, True, True, True, False]), max_flag_gap=2),
                                   np.array([False, False, True, True, True, True]))
 
 
