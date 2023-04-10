8f14199d07d74b408595aaf02e56ad960ecbb17c
Josh Dillon <jsdillon@berkeley.edu>
2023-04-10 16:01:50 -0700

    make exclusion/inclusion consistent

--- hera_qm/time_series_metrics.py
+++ hera_qm/time_series_metrics.py
@@ -52,7 +52,7 @@ def metric_convolution_flagging(metric, starting_flags, ok_range, sigma=30, max_
         metric: 1D numpy array of floats containing the per-file metric values used for flagging.
         starting_flags: 1D numpy array of booleans of initial flags
         ok_range: length-2 tuple of metric range outside of which flags are flags are considered bad
-        sigma: standard deviation of metric Gaussian smoothing scale (in units of files)
+        sigma: standard deviation of metric Gaussian smoothing scale (in units of integrations)
         max_flag_gap: integer maximum allowed sequential flags (default 30)
     Returns:
         new_flags: starting flags, but with possible additional flags
@@ -62,7 +62,7 @@ def metric_convolution_flagging(metric, starting_flags, ok_range, sigma=30, max_
     kernel = np.exp(-np.arange(-len(metric) // 2, len(metric) // 2 + 1)**2 / 2 / sigma**2)
     kernel /= np.sum(kernel)
     convolved_metric = convolve(metric, kernel, mode='reflect')
-
+    
     new_flags = np.array(starting_flags)
     nflags = np.sum(new_flags)
     while True:
@@ -76,12 +76,12 @@ def metric_convolution_flagging(metric, starting_flags, ok_range, sigma=30, max_
         for bs in bad_stretches:
             if np.any(convolved_metric[bs] > ok_range[1]):
                 not_persistant_bad[bs] = False
+    
         for stretch in true_stretches(not_persistant_bad):
-            if not np.any((convolved_metric[stretch] > ok_range[0]) & (convolved_metric[stretch] < ok_range[1])):
-                new_flags[stretch] = True            
-
+            if not np.any((convolved_metric[stretch] >= ok_range[0]) & (convolved_metric[stretch] <= ok_range[1])):
+                new_flags[stretch] = True
+                
         impose_max_flag_gap(new_flags, max_flag_gap=max_flag_gap)
-            
         # check if flags haven't grown and if so, break
         if np.sum(new_flags) == nflags:
             return new_flags
