8ddee52a97c2f91801eb77c1a7399bac856a0c81
Steven Murray <steven.g.murray@asu.edu>
2024-02-15 14:54:30 +0100

    woops: forgot ant_metrics

--- hera_qm/scripts/ant_metrics_run.py
+++ hera_qm/scripts/ant_metrics_run.py
@@ -7,17 +7,21 @@ from hera_qm import utils
 from hera_qm import ant_metrics
 import sys
 
-ap = utils.get_metrics_ArgumentParser('ant_metrics')
-args = ap.parse_args()
-history = ' '.join(sys.argv)
-ant_metrics.ant_metrics_run(args.sum_files,
-                            diff_files=args.diff_files,
-                            apriori_xants=args.apriori_xants,
-                            a_priori_xants_yaml=args.a_priori_xants_yaml,
-                            crossCut=args.crossCut,
-                            deadCut=args.deadCut,
-                            metrics_path=args.metrics_path,
-                            extension=args.extension,
-                            overwrite=args.clobber,
-                            verbose=args.verbose,
-                            history=history)
+def main():
+    ap = utils.get_metrics_ArgumentParser('ant_metrics')
+    args = ap.parse_args()
+    history = ' '.join(sys.argv)
+    ant_metrics.ant_metrics_run(args.sum_files,
+                                diff_files=args.diff_files,
+                                apriori_xants=args.apriori_xants,
+                                a_priori_xants_yaml=args.a_priori_xants_yaml,
+                                crossCut=args.crossCut,
+                                deadCut=args.deadCut,
+                                metrics_path=args.metrics_path,
+                                extension=args.extension,
+                                overwrite=args.clobber,
+                                verbose=args.verbose,
+                                history=history)
+
+if __name__ == '__main__':
+    main()
