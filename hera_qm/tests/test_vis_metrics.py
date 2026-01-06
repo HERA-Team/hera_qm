a341087ae1460a16d92f8b09ccaabe6745e42478
Josh Dillon <jsdillon@berkeley.edu>
2026-01-06 13:54:23 -0800

    Fix test warnings: update LST arrays and add filterwarnings for pyuvdata warnings

--- hera_qm/tests/test_vis_metrics.py
+++ hera_qm/tests/test_vis_metrics.py
@@ -31,7 +31,9 @@ def vismetrics_data():
         while data.Ntimes < 90:
             d2 = copy.deepcopy(data)
             d2.time_array += d2.time_array.max() + d2.integration_time / (24 * 3600)
+            d2.set_lsts_from_time_array()
             data += d2
+        data.set_lsts_from_time_array()
     ntimes = data.Ntimes
     nchan = data.Nfreqs
     data1 = qmtest.noise(size=(ntimes, nchan))
